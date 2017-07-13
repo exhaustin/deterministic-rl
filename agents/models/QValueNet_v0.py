6abe80bf2d20d0429147197ec697a410ea28a002
ExhAustin <austinswang@hotmail.com>
2017-07-13 13:36:15 +0800

    tried dropout, too slow

--- agents/models/QValueNet_v0.py
+++ agents/models/QValueNet_v0.py
@@ -6,7 +6,7 @@ from keras.models import Sequential, Model
 #from keras.engine.traning import collect_trainable_weights
 from keras import layers
 from keras.layers import Input
-from keras.layers.core import Dense, Activation
+from keras.layers.core import Dense, Dropout
 from keras.optimizers import Adam
 import tensorflow as tf
 import keras.backend as K
@@ -52,16 +52,23 @@ class QValueNetwork:
 		K_INIT = initializers.TruncatedNormal(mean=0.0, stddev=1e-3)
 		K_REG = regularizers.l2(1e-3)
 
+		# for dropout
+		#K.set_learning_phase(1)
+
 		#print('Building critic model...')
 		S = Input(shape=[state_dim])
 		w1 = Dense(self.HIDDEN1_UNITS, activation='elu', kernel_initializer=K_INIT, kernel_regularizer=K_REG)(S)
+		#w1_d = Dropout(rate=0.1)(w1)
 		w2 = Dense(self.HIDDEN1_UNITS, activation='linear', kernel_initializer=K_INIT, kernel_regularizer=K_REG)(w1)
+		#w2_d = Dropout(rate=0.1)(w2)
 
 		A = Input(shape=[action_dim])
 		a1 = Dense(self.HIDDEN2_UNITS, activation='linear', kernel_initializer=K_INIT, kernel_regularizer=K_REG)(A)
+		#a1_d = Dropout(rate=0.1)(a1)
 
 		h1 = layers.concatenate([w2,a1])
 		h2 = Dense(self.HIDDEN2_UNITS, activation='elu', kernel_initializer=K_INIT, kernel_regularizer=K_REG)(h1)
+		#h2_d = Dropout(rate=0.1)(h2)
 		V = Dense(1, activation='linear')(h2) 
 		model = Model(inputs=[S,A], outputs=V)
 
@@ -92,10 +99,12 @@ class QValueNetwork:
 		return self.model.train_on_batch(inputs, q_targets)
 
 	def predict(self, inputs):
+		#K.set_learning_phase(0)
 		if self.TAU > 0:
 			return self.target_model.predict(inputs)
 		else:
 			return self.model.predict(inputs)
+		#K.set_learning_phase(1)
 
 	def target_train(self):
 		if self.TAU > 0:
